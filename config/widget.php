6c2e893ad244e0b1f245b7e115bae5e7be993a40
Clayton D'Mello <clayton@chargefield.com>
2019-10-21 18:59:32 -0400

    Update the widget config

--- config/widget.php
+++ config/widget.php
@@ -2,6 +2,26 @@
 
 return [
 
-    //
+    /*
+    |--------------------------------------------------------------------------
+    | Class Namespace
+    |--------------------------------------------------------------------------
+    |
+    | This value sets the base namespace for widget classes in your app.
+    |
+    */
+
+    'class_namespace' => 'App\\Http\\Widgets',
+
+    /*
+    |--------------------------------------------------------------------------
+    | View Path
+    |--------------------------------------------------------------------------
+    |
+    | This value sets the path for widget views.
+    |
+    */
+
+    'view_path' => resource_path('views/widgets'),
 
 ];
