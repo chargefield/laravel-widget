22b274c0a07ea02cf3989a46f903fd41b2994c0a
Clayton D'Mello <clayton@chargefield.com>
2019-10-21 13:43:42 -0400

    Update TestCase to register package providers

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,21 @@
 namespace Chargefield\LaravelWidget\Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use Chargefield\LaravelWidget\WidgetServiceProvider;
 
 class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            WidgetServiceProvider::class,
+        ];
+    }
 }
